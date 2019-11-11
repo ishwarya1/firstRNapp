e1603d372aa3ea01053a3ccb40a8df21a2f5eab0
18016638 <18016638@myrp.edu.sg>
2019-11-11 15:26:33 +0800

    Lesson 04 Exercise

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
+  console.log('I am a console statement');
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -41,21 +42,25 @@ const App: () => React$Node = () => {
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text>Welcome to C308 Web Frameworks!</Text>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>Recommended Eats @RP</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                <Text style={styles.highlight}>W6 Level 1 , E-canteen
+                </Text>Waffles with chocolate
+                dip, Belgian Waffles, Fruit Fondue, Royal
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
+              <Text style={styles.sectionTitle}>ice cream</Text>
+              <Text style={styles.sectionDescription}><Text style={styles.highlight}>E6 Level 1 , E-canteen
+              </Text>This was my first time visiting Holland Village for something other than beer. Surprisingly, I still left extremely happy, full, and a little tipsy! While David manages the operational side of the cafe, his father handles the production of ice-cream.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
+              <Text style={styles.sectionTitle}>Western</Text>
+              <Text style={styles.sectionDescription}><Text style={styles.highlight}>E6 Level 1 , E-canteen
+              </Text>However, whenever my friends and I talk about affordable, value-for-money Western food, Astons is always the first place that comes to mind. However, I’m here to show you that there are plenty of alternatives in our hawker centres that are cheaper (and might even be better) than Astons!
+
+
                 <DebugInstructions />
               </Text>
             </View>
